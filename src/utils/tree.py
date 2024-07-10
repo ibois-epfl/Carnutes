ed076ec747eaada15ca8df41d786fe2e9a6bd931
DamienGilliard <damien.gilliard@epfl.ch>
2024-07-11 01:37:27 +0200

    WIP: documentation of Tree class

--- src/utils/tree.py
+++ src/utils/tree.py
@@ -1,5 +1,11 @@
 import persistent
-
+"""
+Tree class to store tree data.
+The point cloud of the tree is stored as a list of points
+:param tree_id: The id of the tree
+:param tree_name: The name of the tree
+:param tree_point_cloud: The point cloud of the tree as a list of lists of 3 coordinates
+"""
 class Tree(persistent.Persistent):
     def __init__(self, tree_id, tree_name, tree_point_cloud):
         self.tree_id = tree_id
