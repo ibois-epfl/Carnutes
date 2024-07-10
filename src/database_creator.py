9722c066d0052b15adff911e8e0607e7c16cdf19
DamienGilliard <damien.gilliard@epfl.ch>
2024-07-11 01:36:50 +0200

    FIX: conversion errors when serializing point cloud

--- src/database_creator.py
+++ src/database_creator.py
@@ -12,7 +12,7 @@ import utils.tree as tree
 
 def create_database():
     
-    database_folder = 'src/database'
+    database_folder = 'database'
     if not os.path.exists(database_folder):
         os.makedirs(database_folder)
 
@@ -22,10 +22,10 @@ def create_database():
     root = connection.root
 
     tree_pc = o3d.io.read_point_cloud("dataset/test_file.ply")
-    tree_pc = tree_pc.voxel_down_sample(voxel_size=2)
+    tree_pc = tree_pc.voxel_down_sample(voxel_size=1)
     tree_list = []
-    for point in tree_pc.points:
-        tree_list.append([point[0], point[1], point[2]])
+    for i in range(len(tree_pc.points)):
+        tree_list.append([float(tree_pc.points[i][0]), float(tree_pc.points[i][1]), float(tree_pc.points[i][2])])
     tree1 = tree.Tree(1, "Tree 1", tree_list)
 
 
