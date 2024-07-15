1b2f6b85c71ed61a256baaea8f17ab0ecbff7f94
DamienGilliard <damien.gilliard@epfl.ch>
2024-07-15 17:21:25 +0200

    UPDATE: drawing of connectivity of elements in function_1

--- src/function_1.py
+++ src/function_1.py
@@ -1,7 +1,26 @@
+"""
+dummy function to test the bones of the project
+this one creates a graph from the selected breps or lines and draws a graph of the connected elements in the rhino scene
+"""
 #! python 3
 # r: igraph
+
+import System.Drawing
+
 import Rhino
+import scriptcontext as sc
+
 from utils import graphs
+
+# Create a new layer
+layer_name = "Layer_for_connectivity_graph"
+layer_index = sc.doc.Layers.Add(layer_name, System.Drawing.Color.FromArgb(255, 50, 255, 50))
+sc.doc.Layers.SetCurrentLayerIndex(layer_index, True)
+
+attributes = Rhino.DocObjects.ObjectAttributes()
+attributes.LayerIndex = layer_index
+attributes.ObjectColor = System.Drawing.Color.FromArgb(255, 100, 100, 100)
+
     
 def main():
     go = Rhino.Input.Custom.GetObject()
@@ -13,7 +32,11 @@ def main():
 
     geometries = [go.Object(i).Geometry() for i in range(go.ObjectCount)]
     g = graphs.ConnectivityGraph(geometries)
-    g.compute_brep_connectivity_graph()
+    centers = g.compute_brep_connectivity_graph()
+    for edge in g.graph.get_edgelist():
+        sc.doc.Objects.AddLine(g.model[edge[0]].GetBoundingBox(False).Center, g.model[edge[1]].GetBoundingBox(False).Center, attributes)
+
+    # sc.doc.Objects.AddPoints(centers)
     print("Graph created with {} vertices and {} edges".format(g.graph.vcount(), g.graph.ecount())) 
 
 if __name__ == "__main__":
--- src/utils/graphs.py
+++ src/utils/graphs.py
@@ -1,7 +1,10 @@
 #! python 3
 #r: igraph
+#r: numpy
 
 import Rhino
+
+import numpy as np
 import igraph as ig
 
 class ConnectivityGraph(object):
@@ -22,17 +25,30 @@ class ConnectivityGraph(object):
 
     def compute_brep_connectivity_graph(self):
         """
-        Compute the connectivity graph of the brep.
+        Compute the connectivity graph of the brep and stores the graph in self.graph.
+        returns: list[Point3d] 
+            the centers of the bounding boxes of the intersections
         """
+        matrix_of_intersections = np.triu(np.ones((len(self.model),len(self.model))),1)
+        print(matrix_of_intersections)
         n_vertices = len(self.model)
         edges = []
+        centers = []
         for i in range(n_vertices):
             for j in range(i+1,n_vertices):
                 result = Rhino.Geometry.Brep.CreateBooleanIntersection(self.model[i], self.model[j], Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance, False)
-                if len(result) > 0:    
+                if result is not None and len(result) > 0:    
                     edges.append([i,j])
+                    for brep in result:
+                        bounding_box = brep.GetBoundingBox(False)
+                        centers.append(bounding_box.Center)
+                else:
+                    continue
+
+                    
         g = ig.Graph(n_vertices, edges)
         self.graph = g
+        return centers
     
     def compute_polyline_connectivity_graph(self):
         """
