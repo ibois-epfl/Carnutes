f67f611d551e9890a262d5b21554dd3b93798381
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-08-27 01:16:08 +0200

    WIP: trim is now a method of Tree class + improvements on matxh skeleton reliability

--- src/packing/packing_manipulations.py
+++ src/packing/packing_manipulations.py
@@ -11,26 +11,26 @@ import utils.tree
 import open3d as o3d
 import numpy as np
 
-def match_skeletons(reference_skeleton: utils.geometry.Pointcloud, target_skeleton: utils.geometry.Pointcloud) -> utils.geometry.Pointcloud:
+def match_skeletons(model_element: utils.geometry.Pointcloud, original_skeleton: utils.geometry.Pointcloud) -> utils.geometry.Pointcloud:
     """"
     match the two skeletons by adapting the target skeleton to the reference skeleton, 
     so the distances between the corresponding points are identical. 
     The number of points in the target_skeleton is thus matched to the number of points in the reference_skeleton.
 
-    :param reference_skeleton: Pointcloud
-        The reference skeleton to match to
+    :param model_element: Pointcloud
+        The model element that gives the relative distances between the points
 
-    :param target_skeleton: Pointcloud
-        The target tree skeleton to modify so it matches the reference skeleton
+    :param original_skeleton: Pointcloud
+        The original tree skeleton to modify so it matches the reference skeleton
 
-    :return: adapted_target_skeleton: Pointcloud
+    :return: adapted_skeleton: Pointcloud
         The adapted target skeleton
     ```
     ref:           target:      adapted_target:
      p                p              p
-      \               or             |
-       p              or             p
-       or             p              |
+      \               |              |
+       p              |              p
+       |              p              |
        p             /               p
       /             /               /
      /             /               /
@@ -38,18 +38,19 @@ def match_skeletons(reference_skeleton: utils.geometry.Pointcloud, target_skelet
     ```
     """
     list_of_reference_distances = []
-    for i in range(len(reference_skeleton.points)-1):
-        dist = np.linalg.norm(np.array(reference_skeleton.points[i]) - np.array(reference_skeleton.points[i+1]))
+    for i in range(len(model_element.points)-1):
+        dist = np.linalg.norm(np.array(model_element.points[i]) - np.array(model_element.points[i+1]))
         list_of_reference_distances.append(dist)
     list_of_target_distances = []
-    for i in range(len(target_skeleton.points)-1):
-        dist = np.linalg.norm(np.array(target_skeleton.points[i]) - np.array(target_skeleton.points[i+1]))
+    for i in range(len(original_skeleton.points)-1):
+        dist = np.linalg.norm(np.array(original_skeleton.points[i]) - np.array(original_skeleton.points[i+1]))
         list_of_target_distances.append(dist)
-    new_target_skeleton = [[target_skeleton.points[0][0],
-                           target_skeleton.points[0][1],
-                           target_skeleton.points[0][2]]]
+    new_skeleton_as_list = [[original_skeleton.points[0][0],
+                           original_skeleton.points[0][1],
+                           original_skeleton.points[0][2]]]
 
     if np.sum(list_of_target_distances) < np.sum(list_of_reference_distances):
+        print("The target skeleton is smaller than the reference skeleton, it cannot be adapted. None is returned. (From match_skeletons method)")
         return None
         
     else:
@@ -60,11 +61,12 @@ def match_skeletons(reference_skeleton: utils.geometry.Pointcloud, target_skelet
                 if reference_cummulative_distance < target_cummulative_distance:
                     break
             ratio = (np.sum(list_of_reference_distances[:i+1]) - np.sum(list_of_target_distances[:j]))/list_of_target_distances[j]
-            new_point =  [target_skeleton.points[j][0] + ratio * (target_skeleton.points[j+1][0] - target_skeleton.points[j][0]), 
-                          target_skeleton.points[j][1] + ratio * (target_skeleton.points[j+1][1] - target_skeleton.points[j][1]), 
-                          target_skeleton.points[j][2] + ratio * (target_skeleton.points[j+1][2] - target_skeleton.points[j][2])]
-            new_target_skeleton.append(new_point)
-        return utils.geometry.Pointcloud(new_target_skeleton)
+            new_point =  [original_skeleton.points[j][0] + ratio * (original_skeleton.points[j+1][0] - original_skeleton.points[j][0]), 
+                          original_skeleton.points[j][1] + ratio * (original_skeleton.points[j+1][1] - original_skeleton.points[j][1]), 
+                          original_skeleton.points[j][2] + ratio * (original_skeleton.points[j+1][2] - original_skeleton.points[j][2])]
+            new_skeleton_as_list.append(new_point)
+        adapted_skeleton = utils.geometry.Pointcloud(new_skeleton_as_list)
+        return utils.geometry.Pointcloud(new_skeleton_as_list)
 
 def perform_icp_registration(target_skeleton : utils.geometry.Pointcloud, source_skeleton : utils.geometry.Pointcloud, max_correspondence_distance : float) -> o3d.pipelines.registration.RegistrationResult:
     """
