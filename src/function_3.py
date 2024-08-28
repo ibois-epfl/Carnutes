724834665378201f9e9e8abdc521c437be43ec15
DamienGilliard <damien.gilliard@epfl.ch>
2024-08-28 02:04:17 +0200

    UPDATE: implement cleanups done to different modules for more flexible and OOP logic

--- src/function_3.py
+++ src/function_3.py
@@ -11,10 +11,10 @@ import os
 import copy
 import System
 
-from utils import model, tree, geometry
+from utils import model, tree, geometry, interact_with_rhino
 from utils import element as elem
 from utils.tree import Tree
-from packing import packing_manipulations, packing_combinatorics
+from packing import packing_combinatorics
 
 import numpy as np
 import Rhino
@@ -46,43 +46,9 @@ def main():
     """
 
     # Create the model
-    go = Rhino.Input.Custom.GetObject()
-    go.SetCommandPrompt("Select the whole structure")
-    go.GeometryFilter = Rhino.DocObjects.ObjectType.Brep | Rhino.DocObjects.ObjectType.Curve
-    go.GetMultiple(1, 1000)
-
-    if go.CommandResult() != Rhino.Commands.Result.Success:
-        print("No object selected.")
-        return
-    
-    geometries = [go.Object(i).Geometry() for i in range(go.ObjectCount)]
-
-    if len(geometries) < 2:
-        print("At least two geometries are needed to create a graph.")
-        return
-    if isinstance(geometries[0], Rhino.Geometry.LineCurve):
-        geometries = [geo.ToNurbsCurve() for geo in geometries]
-        print("LineCurves converted to NurbsCurves")
-    elif isinstance(geometries[0], Rhino.Geometry.Line):
-        geometries = [geo.ToNurbsCurve() for geo in geometries]
-        print("Lines converted to NurbsCurves")
-    elif isinstance(geometries[0], Rhino.Geometry.Curve):
-        geometries = [geo.ToNurbsCurve() for geo in geometries]
-        print("Curves converted to NurbsCurve")
-
-    elements = [elem.Element(geometries[i], go.Object(i).ObjectId) for i in range(go.ObjectCount)]
-    current_model = model.Model(elements)
-
+    current_model = interact_with_rhino.create_model_from_rhino_selection()
     # Ask user which element (s)he wants to replace with a point cloud
-    go = Rhino.Input.Custom.GetObject()
-    go.SetCommandPrompt("Select the element to replace with a point cloud")
-    go.GeometryFilter = Rhino.DocObjects.ObjectType.Brep | Rhino.DocObjects.ObjectType.Curve
-    go.GetMultiple(1, 1)
-    if go.CommandResult() != Rhino.Commands.Result.Success:
-        print("No object selected.")
-        return
-    element = go.Object(0).Geometry()
-    element_guid = go.Object(0).ObjectId
+    element_geometry, element_guid = interact_with_rhino.select_single_element_to_replace()
 
     for element in current_model.elements:
         if element.GUID == element_guid:
@@ -98,22 +64,8 @@ def main():
                 reference_pc_as_list.append(edge["location"])
             break
     # at this point the reference_pc_as_list should contain the points, but they are not ordered. We need to order them.
-    # for this we assume that an element in the model has a principal direction, and we sort the points along this direction
-    x_max_bounds = max([point[0] for point in reference_pc_as_list])
-    x_min_bounds = min([point[0] for point in reference_pc_as_list])
-    delta_x = x_max_bounds - x_min_bounds
-    y_max_bounds = max([point[1] for point in reference_pc_as_list])
-    y_min_bounds = min([point[1] for point in reference_pc_as_list])
-    delta_y = y_max_bounds - y_min_bounds
-    z_max_bounds = max([point[2] for point in reference_pc_as_list])
-    z_min_bounds = min([point[2] for point in reference_pc_as_list])
-    delta_z = z_max_bounds - z_min_bounds
-    if delta_x > delta_y and delta_x > delta_z:
-        reference_pc_as_list = sorted(reference_pc_as_list, key=lambda x: x[0])
-    elif delta_y > delta_x and delta_y > delta_z:
-        reference_pc_as_list = sorted(reference_pc_as_list, key=lambda x: x[1])
-    elif delta_z > delta_x and delta_z > delta_y:
-        reference_pc_as_list = sorted(reference_pc_as_list, key=lambda x: x[2])
+    reference_pc_as_list = geometry.sort_points(reference_pc_as_list)
+
     # Retrieve the best fitting tree from the database
     reference_skeleton = geometry.Pointcloud(reference_pc_as_list)
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -124,14 +76,7 @@ def main():
     my_tree.align_to_skeleton(reference_skeleton)
 
     # Brep to crop the point cloud
-    if isinstance(target.geometry, Rhino.Geometry.NurbsCurve):
-        cylinder = Rhino.Geometry.Brep.CreatePipe(target.geometry, 1, True, Rhino.Geometry.PipeCapMode.Flat, True, 0.01, 0.01)[0]
-    elif isinstance(target.geometry, Rhino.Geometry.Brep):
-        edges = [target.geometry.Edges[i] for i in range(target.geometry.Edges.Count) if not target.geometry.Edges[i].IsClosed]
-        cylinder = Rhino.Geometry.Brep.CreatePipe(edges[0], 1, True, Rhino.Geometry.PipeCapMode.Flat, True, 0.01, 0.01)[0]
-        # cylinder = Rhino.Geometry.Brep.CreatePipe(reference_crv_for_brep, 1, True, Rhino.Geometry.PipeCapMode.Flat, True, 0.01, 0.01)[0]
-    else:
-        raise ValueError("The geometry of the target element is not supported.")
+    cylinder = target.create_bounding_cylinder(radius=1)
     crop(my_tree, cylinder)
     my_tree.create_mesh()
 
