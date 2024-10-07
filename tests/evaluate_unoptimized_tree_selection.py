FIX: test result values rounded and units added

tests/evaluate_unoptimized_tree_selection.py
@@ -101,8 +101,9 @@ def evaluate_unoptimized_tree_selection(reference_diameter: float = 0.2):
     csv_file_treewise.close()
 
     mean_RMSE = np.mean(RMSEs)
+    np.round(mean_RMSE, 4)
     with open("rmse_result.txt", "w") as f:
-        f.write(f"{mean_RMSE}")
+        f.write(f"{mean_RMSE} [m]")
 
     mean_tree_usage = np.mean(
         [
@@ -110,8 +111,10 @@ def evaluate_unoptimized_tree_selection(reference_diameter: float = 0.2):
             for tree_id in tree_number_usage
         ]
     )
+    np.round(mean_tree_usage, 4)
+    mean_tree_usage = mean_tree_usage * 100
     with open("tree_usage_result.txt", "w") as f:
-        f.write(f"{mean_tree_usage}")
+        f.write(f"{mean_tree_usage} [%]")
 
 
 if __name__ == "__main__":