FIX: compute initial rotation before ICP for more robustness, and trickle down of rotation to initial function

src/packing/packing_combinatorics.py
@@ -50,9 +50,12 @@ def compute_best_tree_element_matching(
         The best fitting segment of the skeleton point cloud
     :return: best_rmse: float
         The rmse of the best fitting segment
+    :return: best_init_rotation: np.array
+        The rotation that was applied to the target skeleton to match the reference once both are reset to the origin
     """
     best_rmse = minimum_rmse
     best_skeleton = None
+    best_init_rotation = None
 
     for i in range(2):
         model_element.points = (
@@ -80,15 +83,17 @@ def compute_best_tree_element_matching(
                 continue
             if adapted_skeleton is None:
                 continue
-            result = packing_manipulations.perform_icp_registration(
+            result, init_rotation = packing_manipulations.perform_icp_registration(
                 model_element, adapted_skeleton, 20.0
             )
             if result.inlier_rmse < best_rmse:
                 best_rmse = result.inlier_rmse
                 best_skeleton = adapted_skeleton
-    if best_rmse is None:
-        return None, None
-    return best_skeleton, best_rmse
+                best_init_rotation = init_rotation
+
+    if best_rmse == minimum_rmse:
+        return None, None, None
+    return best_skeleton, best_rmse, best_init_rotation
 
 
 def find_best_tree_unoptimized(
@@ -126,6 +131,7 @@ def find_best_tree_unoptimized(
     reader = db_reader.DatabaseReader(database_path)
     n_tree = reader.get_num_trees()
     best_tree = None
+    best_init_rotation = None
 
     # initiallize the best rmse to infinity before the first iteration
     best_db_level_rmse = np.inf
@@ -146,6 +152,7 @@ def find_best_tree_unoptimized(
         (
             best_skeleton_segment,
             best_tree_level_rmse,
+            init_rotation,
         ) = compute_best_tree_element_matching(
             model_element, reference_diameter, tree, np.inf
         )
@@ -158,6 +165,7 @@ def find_best_tree_unoptimized(
             best_db_level_rmse = best_tree_level_rmse
             best_tree = tree
             best_skeleton = best_skeleton_segment
+            best_init_rotation = init_rotation
 
         if (
             best_db_level_rmse < 0.01
@@ -195,12 +203,13 @@ def find_best_tree_unoptimized(
                 selected_tree,
                 best_skeleton_segment,
                 best_db_level_rmse,
+                best_init_rotation,
             )
         return selected_tree
     else:
         reader.close()
         print("No tree found in find_best_tree, returning None")
-        return None, None, None
+        return None, None, None, None
 
 
 def find_best_tree_optimized(
@@ -238,6 +247,8 @@ def find_best_tree_optimized(
         The best fitting segment of the target point cloud. Only returned if return_rmse is True.
     :return: rmse: float
         The rmse of the best fitting tree. Only returned if return_rmse is True.
+    :return: best_init_rotation: np.array
+        The best initial rotation that was applied to the target skeleton to match the reference once both are reset to the origin
     """
     # unpack the database:
     reader = db_reader.DatabaseReader(database_path)
@@ -249,6 +260,7 @@ def find_best_tree_optimized(
     trees = []
     skeleton_segments = []
     tree_ids = []
+    best_init_rotations = []
 
     # iterate over the trees in the database
     for i in range(n_tree):
@@ -265,12 +277,14 @@ def find_best_tree_optimized(
         (
             best_skeleton_segment,
             best_tree_level_rmse,
+            best_init_rotation,
         ) = compute_best_tree_element_matching(model_element, tree.skeleton, np.inf)
 
         if best_tree_level_rmse is not None and best_tree_level_rmse is not np.inf:
             rmse.append(best_tree_level_rmse)
             trees.append(tree)
             skeleton_segments.append(best_skeleton_segment)
+            best_init_rotations.append(best_init_rotation)
             tree_ids.append(i)
 
     if len(rmse) == 0:
@@ -279,9 +293,15 @@ def find_best_tree_optimized(
             f"No tree were found in the database, but {optimisation_basis} are required"
         )
         return None, None, None
-    (sorted_rmse, sorted_trees, sorted_skeleton_segments, sorted_tree_ids,) = zip(
+    (
+        sorted_rmse,
+        sorted_trees,
+        sorted_skeleton_segments,
+        sorted_tree_ids,
+        sorted_best_init_rotation,
+    ) = zip(
         *sorted(
-            zip(rmse, trees, skeleton_segments, tree_ids),
+            zip(rmse, trees, skeleton_segments, tree_ids, best_init_rotations),
             key=lambda x: rmse,
         )
     )
@@ -289,15 +309,22 @@ def find_best_tree_optimized(
     n_best_rmse = sorted_rmse[:optimisation_basis]
     n_best_skeleton = sorted_skeleton_segments[:optimisation_basis]
     n_best_tree_ids = sorted_tree_ids[:optimisation_basis]
-
+    n_best_init_rotations = sorted_best_init_rotation[:optimisation_basis]
     (
         sorted_best_tree,
         sorted_best_db_level_rmse,
         sorted_best_skeleton,
         sorted_best_tree_id,
+        sorted_best_init_rotation,
     ) = zip(
         *sorted(
-            zip(n_best_trees, n_best_rmse, n_best_skeleton, n_best_tree_ids),
+            zip(
+                n_best_trees,
+                n_best_rmse,
+                n_best_skeleton,
+                n_best_tree_ids,
+                n_best_init_rotations,
+            ),
             key=lambda x: x[0].height,
         )
     )  # get the tree with the smallest height among the five best fitting trees
@@ -306,6 +333,7 @@ def find_best_tree_optimized(
     best_db_level_rmse = sorted_best_db_level_rmse[0]
     best_skeleton = sorted_best_skeleton[0]
     best_tree_id = sorted_best_tree_id[0]
+    best_init_rotation = sorted_best_init_rotation[0]
 
     if best_tree is not None:
         # remove the best tree from the database
@@ -338,6 +366,7 @@ def find_best_tree_optimized(
                 selected_tree,
                 best_skeleton_segment,
                 best_db_level_rmse,
+                best_init_rotation,
             )
         reader.close()
         return selected_tree
@@ -345,7 +374,7 @@ def find_best_tree_optimized(
     else:
         reader.close()
         print("No tree found in find_best_tree, returning None")
-        return None, None, None, None
+        return None, None, None, None, None
 
 
 def tree_based_iterative_matching(

src/packing/packing_manipulations.py
@@ -6,6 +6,7 @@ import copy
 
 import utils.database_reader as db_reader
 import utils.geometry
+import utils.geometrical_operations
 import utils.tree
 
 import open3d as o3d
@@ -113,6 +114,8 @@ def perform_icp_registration(
 
     :return: result: o3d.pipelines.registration.RegistrationResult
         The result of the icp registration
+    :return: initial_rotation: np.array
+        The initial rotation matrix used for the icp registration. It aligns the skeleton to the reference once both have been re-located to the origin.
     """
     source_pc = o3d.geometry.PointCloud()
     source_pc.points = o3d.utility.Vector3dVector(np.array(source_skeleton.points))
@@ -122,25 +125,27 @@ def perform_icp_registration(
     target_pc.points = o3d.utility.Vector3dVector(np.array(target_skeleton.points))
     target_pc.estimate_normals()
 
-    # initial translation
-    initial_translation = np.identity(4)
-    translation_vector = np.mean(target_skeleton.points, axis=0) - np.mean(
-        source_skeleton.points, axis=0
+    # translation to origin
+    source_pc.translate(-source_pc.points[0])
+    target_pc.translate(-target_pc.points[0])
+
+    # initial rotation
+    initial_rotation = (
+        utils.geometrical_operations.find_rotation_matrix_between_skeletons(
+            target_pc, source_pc
+        )
     )
-    initial_translation[0, 3] = translation_vector[0]
-    initial_translation[1, 3] = translation_vector[1]
-    initial_translation[2, 3] = translation_vector[2]
 
     convergence_criteria = o3d.pipelines.registration.ICPConvergenceCriteria(
         max_iteration=5,
-        relative_rmse=1e-3,
+        relative_rmse=1e-6,
     )
 
     result = o3d.pipelines.registration.registration_icp(
         source=source_pc,
         target=target_pc,
         max_correspondence_distance=max_correspondence_distance,
-        init=initial_translation,
+        init=initial_rotation,
         criteria=convergence_criteria,
     )
-    return result
+    return result, result.transformation

src/utils/tree.py
@@ -119,11 +119,13 @@ class Tree(persistent.Persistent):
 
         return self.skeleton
 
-    def align_to_skeleton(self, reference_skeleton):
+    def align_to_skeleton(self, reference_skeleton, initial_rotation=None):
         """
         Align the tree to a reference skeleton using ICP
         :param reference_skeleton: Pointcloud
             The reference skeleton to align to
+        :param initial_rotation: np.array (4,4), optional
+            The initial rotation matrix to use for the ICP registration. None by default.
         """
         tree_pc = o3d.geometry.PointCloud()
         tree_pc.points = o3d.utility.Vector3dVector(np.array(self.point_cloud.points))
@@ -134,35 +136,43 @@ class Tree(persistent.Persistent):
         reference_pc.points = o3d.utility.Vector3dVector(
             np.array(reference_skeleton.points)
         )
+        reference_pc.estimate_normals()
 
-        # all_points_ref = np.asarray(reference_pc.points)
-        # for i in range(len(all_points_ref)):
-        #     print(np.asarray(all_points_ref[i]))
-        # print("And using as skeleton: ", skeleton_pc, "with points:")
-        # all_points_skel = np.asarray(skeleton_pc.points)
-        # for i in range(len(all_points_skel)):
-        #     print(np.asarray(all_points_skel[i]))
+        t_origin_to_reference = np.eye(4)
+        t_origin_to_reference[0, 3] = reference_pc.points[0][0]
+        t_origin_to_reference[1, 3] = reference_pc.points[0][1]
+        t_origin_to_reference[2, 3] = reference_pc.points[0][2]
 
-        initial_translation = np.identity(4)
-        initial_translation[:3, 3] = np.mean(
-            np.asarray(reference_pc.points), axis=0
-        ) - np.mean(np.asarray(skeleton_pc.points), axis=0)
-        tree_pc.transform(initial_translation)
+        t_skeleton_to_origin = np.eye(4)
+        t_skeleton_to_origin[0, 3] = -skeleton_pc.points[0][0]
+        t_skeleton_to_origin[1, 3] = -skeleton_pc.points[0][1]
+        t_skeleton_to_origin[2, 3] = -skeleton_pc.points[0][2]
+
+        tree_pc.translate(-skeleton_pc.points[0])
+        skeleton_pc.translate(-skeleton_pc.points[0])
+        reference_pc.translate(-reference_pc.points[0])
+
+        initial_rotation = find_rotation_matrix_between_skeletons(
+            reference_pc, skeleton_pc
+        )
 
         convergence_criteria = o3d.pipelines.registration.ICPConvergenceCriteria(
-            relative_fitness=1.000000e-03, max_iteration=40, relative_rmse=1.000000e-03
+            max_iteration=5, relative_rmse=1.000000e-06
         )
 
         result = o3d.pipelines.registration.registration_icp(
             source=skeleton_pc,
             target=reference_pc,
-            init=initial_translation,
-            max_correspondence_distance=10.0,
+            init=initial_rotation,
+            max_correspondence_distance=20.0,
             criteria=convergence_criteria,
         )
-
-        transformation = result.transformation
-        # print("Transformation matrix is ", transformation)
+        print(
+            f"rmse is {result.inlier_rmse}  and fitness is {result.fitness} after latest icp in tree.py, align skeletons of {len(skeleton_pc.points)} and {len(reference_pc.points)} points"
+        )
+        transformation = np.dot(
+            t_origin_to_reference, np.dot(result.transformation, t_skeleton_to_origin)
+        )
         tree_pc = copy.deepcopy(tree_pc)
 
         # Assuming an affine transformation
@@ -232,6 +242,7 @@ class Tree(persistent.Persistent):
         new_skeleton = []
         new_points = []
         new_colors = []
+        new_circles = []
         if delta_x > delta_y and delta_x > delta_z:  # meaning that the main axis is x
             for i, point in enumerate(self.point_cloud.points):
                 if point[0] < x_min_bounds or x_max_bounds < point[0]:
@@ -240,6 +251,9 @@ class Tree(persistent.Persistent):
             for point in self.skeleton.points:
                 if point[0] < x_min_bounds or x_max_bounds < point[0]:
                     new_skeleton.append(point)
+            for circle in self.skeleton_circles:
+                if circle[0][0] < x_min_bounds or x_max_bounds < circle[0][0]:
+                    new_circles.append(circle)
         elif delta_y > delta_x and delta_y > delta_z:  # meaning that the main axis is y
             for i, point in enumerate(self.point_cloud.points):
                 if point[1] < y_min_bounds or y_max_bounds < point[1]:
@@ -248,6 +262,9 @@ class Tree(persistent.Persistent):
             for point in self.skeleton.points:
                 if point[1] < y_min_bounds or y_max_bounds < point[1]:
                     new_skeleton.append(point)
+            for circle in self.skeleton_circles:
+                if circle[0][1] < y_min_bounds or y_max_bounds < circle[0][1]:
+                    new_circles.append(circle)
         elif delta_z > delta_x and delta_z > delta_y:  # meaning that the main axis is z
             for i, point in enumerate(self.point_cloud.points):
                 if point[2] < z_min_bounds or z_max_bounds < point[2]:
@@ -256,9 +273,13 @@ class Tree(persistent.Persistent):
             for point in self.skeleton.points:
                 if point[2] < z_min_bounds or z_max_bounds < point[2]:
                     new_skeleton.append(point)
+            for circle in self.skeleton_circles:
+                if circle[0][2] < z_min_bounds or z_max_bounds < circle[0][2]:
+                    new_circles.append(circle)
         self.point_cloud.points = new_points
         self.point_cloud.colors = new_colors
         self.skeleton.points = new_skeleton
+        self.skeleton_circles = new_circles
 
         if len(self.point_cloud.points) > 1:
             self.height = max([point[2] for point in self.point_cloud.points]) - min(